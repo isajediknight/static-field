17a001b2233dc6a1a01da8c4f2cb8f5bc55e4cdf
isajediknight <luke@isajediknight.com>
2023-01-22 00:15:22 -0600

    [master] favicon and user-agent and ip address logic

--- node/server/use_express.js
+++ node/server/use_express.js
@@ -1,10 +1,29 @@
 const express = require('express')
+var fs = require('fs');
 const app = express()
+var useragent = require('express-useragent');
+
 
 function print_debug_to_console(req) {
-  console.log(req.header('User-Agent'))
+  //console.log(getOs(req.header('User-Agent')))
   console.log(req.url)
+  //console.log(JSON.stringify(req.useragent['browser']))
 }
+
+const ip = require('ip')
+
+
+
+console.log(" Local Server IP Address: " + ip.address())
+
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+// Favicon
+app.use('/favicon.ico', express.static('../images/favicon.ico'));
+// User Agent Helper
+app.use(useragent.express());
+
 
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html')
@@ -21,12 +40,19 @@ app.get('/fun', (req, res) => {
   print_debug_to_console(req)
 });
 
+
+app.get('/show-user-agent', (req, res) => {
+  res.set('Content-Type', 'text/html')
+  res.send(req.useragent);
+  print_debug_to_console(req)
+});
+
 app.get('*', function(req, res){
   res.set('Content-Type', 'text/html')
   res.status(404).send('Page Not Found')
   print_debug_to_console(req)
 });
 
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log('Server ready')
 });
